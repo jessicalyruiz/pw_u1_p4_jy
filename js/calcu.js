ec432fa97557640f9cf083f6b07d16ccfaf1990c
jessicalyruiz <jessicalyr@gmail.com>
2023-05-24 19:44:21 -0500

    taller 9 js externo

---
+++ js/calcu.js
@@ -0,0 +1,42 @@
+function sumar(num1,num2){
+    return num1+num2;
+}
+
+
+
+function restar(num1,num2){
+    return num1-num2;
+}
+
+function multiplicar(num1,num2){
+    return num1+num2;
+}
+
+
+function dividir(num1,num2){
+    return num1-num2;
+}
+
+function operarNumeros(operacion){
+var numero1=convertir('n1');
+var numero2=convertir('n2');
+switch(operacion){
+    case 'suma':
+
+            document.getElementById('lblresult').innerHTML='Resultado: ' +sumar(numero1,numero2);
+            break;
+}
+}
+
+function convertir(idCampo){
+    return parseInt(document.getElementById(idCampo).value);
+   }
+
+
+   function eliminarElemento(){
+    document.getElementById('lbleliminar').remove;
+   }
+
+   function insertarElemento(){
+    document.getElementById('element').innerHTML="<strong>Importante</strong>";
+   }
