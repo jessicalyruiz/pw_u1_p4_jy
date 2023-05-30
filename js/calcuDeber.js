aa4468611f0c11090dd87af678f525ef8de2d8bc
jessicaly <jessiclyaruiz@hotmail.com>
2023-05-29 21:55:47 -0500

    calcu completa

--- js/calcuDeber.js
+++ js/calcuDeber.js
@@ -15,29 +15,50 @@ const dividir=(num1,num2)=>{
 const operarNumeros=()=>{
     console.log('operar numeros')
     let operacion=llenar('=');
-    let num1='',num2='';
+    let num1='',num2='',simbolo='';
     let op=false;
     for(let i=0; i<operacion.length;i++){
         console.log('for operar numeros : '+i)
-        if(i!='+' && i!='-'&& i!='*'&&i!='/'&&!op){
+        if(operacion[i]!=='+' && operacion[i]!='-'&& operacion[i]!='*'&&operacion[i]!='/'&&!op){
             num1+=operacion[i]
-            console.log('operar numeros if: '+num1)
+            console.log('num1: '+num1)
         }else{
-                console.log('operar numeros else')
+            if(op){
+                if(operacion[i]=='=') break;
+                num2+=operacion[i]
+                console.log('num2 '+num2)
+            }else{
+                console.log('operar numeros else, operacion ')
+                simbolo=operacion[i]
+                console.log('operacion '+simbolo)
+                op=true;
+              }
         }
     }
-    //let numero1=convertir('n1');
-    //let numero2=convertir('n2');
-    switch(operacion){
-        case 'suma':
-            document.getElementById('')
-
+    let numero1=parseFloat(num1);
+    let numero2=parseFloat(num2);
+    switch(simbolo){
+        case '/':
+            console.log('dividir')
+           let r= dividir(numero1,numero2)
+           console.log(r)
+            document.getElementById('resultado').value=r
+            break;
+        case '*':
+            document.getElementById('resultado').value=multiplicar(numero1,numero2)
+            break;
+        case '+':
+            document.getElementById('resultado').value=sumar(numero1,numero2)
+            break;
+        case '-':
+            document.getElementById('resultado').value=restar(numero1,numero2)
+            break;
     }
 }
-
+/*
 const convertir=(idcampo)=>{
     return parseFloat(document.getElementById(idcampo).value);
-}
+}*/
 
 const llenar=(dato)=>{
     console.log('llenar '+dato)
@@ -45,4 +66,12 @@ const llenar=(dato)=>{
     operacion.value+=dato;
     console.log(operacion.value)
     return operacion.value
+}
+
+
+
+
+const vaciar=()=>{
+    document.getElementById('pantalla').value='';
+    document.getElementById('resultado').value=''
 }
